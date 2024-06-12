def73445bbb6641c4268320cc8616298f0f3bf4f
Manuel Boesl <manuel.boesl@gmail.com>
2024-06-12 17:17:09 +0200

    Added simple reading of vehicle and traffic data

--- read_cm_quantities.py
+++ read_cm_quantities.py
@@ -26,6 +26,22 @@ cm.subscribe(vehspd)
 # Let's also read the simulation status (simulation status is not a quantity but a command
 # so the command parameter must be set to True)
 sim_status = Quantity("SimStatus", Quantity.INT, True)
+vhcl_pos_x = Quantity("Vhcl.Fr1.x", Quantity.FLOAT)
+vhcl_pos_y = Quantity("Vhcl.Fr1.y", Quantity.FLOAT)
+vhcl_pos_z = Quantity("Vhcl.Fr1.z", Quantity.FLOAT)
+
+pedestrian_x = Quantity("Traffic.T01.tx", Quantity.FLOAT)
+pedestrian_y = Quantity("Traffic.T01.ty", Quantity.FLOAT)
+pedestrian_z = Quantity("Traffic.T01.tz", Quantity.FLOAT)
+
+cm.subscribe(vhcl_pos_x)
+cm.subscribe(vhcl_pos_y)
+cm.subscribe(vhcl_pos_z)
+
+cm.subscribe(pedestrian_x)
+cm.subscribe(pedestrian_y)
+cm.subscribe(pedestrian_z)
+
 cm.subscribe(sim_status)
 
 # 6 - Read all subscribed quantities. In this example, vehicle speed and simulation status
@@ -35,7 +51,17 @@ cm.read()
 cm.read()
 time.sleep(0.1)
 c = 5
-while(c > 0):
+
+print("Waiting for CarMaker to start the sim")
+cm.read()
+while(cm.status_dic.get(sim_status.data) != "Preprocessing"):
+    cm.read()
+    print(cm.status_dic.get(sim_status.data))
+    time.sleep(0.1)
+
+print("Sim Started")
+
+while(cm.status_dic.get(sim_status.data) != "Idle"):
     c = c - 1
     # Read data from carmaker
     cm.read()
@@ -43,4 +69,13 @@ while(c > 0):
     print("Vehicle speed: " + str(vehspd.data * 3.6) + " km/h")
     print("Simulation status: " + ("Running" if sim_status.data >=
                                    0 else cm.status_dic.get(sim_status.data)))
+    
+    print(f"Vhcl x: {vhcl_pos_x.data}")
+    print(f"Vhcl y: {vhcl_pos_y.data}")
+    print(f"Vhcl z: {vhcl_pos_z.data}")
+
+    print(f"Ped x: {pedestrian_x.data}")
+    print(f"Ped y: {pedestrian_y.data}")
+    print(f"Ped z: {pedestrian_z.data}")
+
     time.sleep(1)
